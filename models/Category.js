b312884301897fbc1dafd57102ea735cd26acaf2
yura <yura@socialfix.com>
2023-05-30 00:15:04 +0300

    category dropdown

--- models/Category.js
+++ models/Category.js
@@ -2,6 +2,7 @@ const { model, models, Schema } = require("mongoose");
 
 const CategorySchema = new Schema({
   name: {type: String, required: true},
+  parent: {type: String}
 });
 
 export const Category = models?.Category || model('Category', CategorySchema);
