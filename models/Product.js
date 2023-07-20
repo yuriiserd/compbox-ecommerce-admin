3164cefc3ee11f11d29f830971cbb8c429882240
yura <yura@socialfix.com>
2023-07-20 17:24:41 +0300

    update category parent name to Object Id

--- models/Product.js
+++ models/Product.js
@@ -1,8 +1,8 @@
-import { Schema, model, models } from "mongoose"
+import mongoose, { Schema, model, models } from "mongoose"
 
 const ProductSchema = new Schema({
   title: {type: String, required: true},
-  category: String,
+  category: {type:mongoose.Types.ObjectId, ref: 'Category'},
   description: String,
   price: {type: Number, required: true},
   images: [{type: String}],
