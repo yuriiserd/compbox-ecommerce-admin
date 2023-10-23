08864e53f85e34778c62318d2bad57160cadfe49
yura <yura@socialfix.com>
2023-10-23 20:01:38 +0300

    category images

--- models/Category.js
+++ models/Category.js
@@ -1,6 +1,7 @@
 import mongoose, { model, models, Schema } from "mongoose"
 
 const CategorySchema = new Schema({
+  image: {type: String},
   name: {type: String, required: true},
   parent: {type:mongoose.Types.ObjectId, ref: 'Category'},
   properties: [{
