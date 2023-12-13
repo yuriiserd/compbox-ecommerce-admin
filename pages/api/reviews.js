a476aab1a633642c99e6ae5db1653dab6a022394
yura <yura@socialfix.com>
2023-12-13 15:02:36 +0200

    Add animation and update Nav component. Update reviews page

--- pages/api/reviews.js
+++ pages/api/reviews.js
@@ -1,12 +1,19 @@
 import { mongooseConnect } from "@/lib/mongoose";
+import { Product } from "@/models/Product";
 import { Review } from "@/models/Review";
 
 export default async function handler(req, res) {
   await mongooseConnect();
+  
   const { method } = req;
   if (method === "GET") {
-    const reviews = await Review.find({}).populate('productId').sort({createdAt: -1});
-    res.status(201).json(reviews);
+    if (req?.query?.status && req?.query?.status !== "status") {
+      const reviews = await Review.find({status: req.query.status}).populate('productId').sort({createdAt: -1});
+      res.status(201).json(reviews);
+    } else {
+      const reviews = await Review.find({}).populate('productId').sort({createdAt: -1});
+      res.status(201).json(reviews);
+    }
   }
   if (method === "PUT") {
     const { id } = req.body;
